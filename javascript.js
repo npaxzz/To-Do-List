29c101a3545c7abbd001ce5e14e0740431632f64
npaxzz <82027125+npaxzz@users.noreply.github.com>
2021-06-05 17:45:54 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,15 +5,21 @@ const toDo = document.querySelector(".to-do");
 function addItem() {
   function stopAdding(e) {
     e.preventDefault();
-    // preventDefault() จะยกเลิกเหตการณ์
+    // preventDefault() จะยกเลิกเหตุการณ์
   }
   //   ถ้าไม่ใส่ข้อความ ให้ alert และ  stopAdding()
   //   ===	equal value and equal type
-  if (input.nodeValue.length === 0) {
+  if (input.value.length === 0) {
     alert("Please Enter Something to add ! ");
     btn.addEventListener("click", stopAdding());
-  } else if (input.nodeValue.length >= 40) {
+  } else if (input.value.length >= 40) {
     alert("Maximum value of letters is 40 !");
     btn.addEventListener("click", stopAdding());
   }
+
+  const div = document.createElement("div");
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+
+  li.innerHTML = input.value;
 }
