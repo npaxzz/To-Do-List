d202f571f1705c431bd0a019c9b45357d16cffe5
npaxzz <82027125+npaxzz@users.noreply.github.com>
2021-06-05 16:47:17 +0700

    function stopAdding()

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 const input = document.querySelector("#inp");
 const btn = document.querySelector("#btn");
 const toDo = document.querySelector(".to-do");
+
+function addItem() {
+  function stopAdding(e) {
+    e.preventDefault();
+    // preventDefault() จะยกเลิกเหตการณ์
+  }
+  //   ถ้าไม่ใส่ข้อความ ให้ alert และ  stopAdding()
+  //   ===	equal value and equal type
+  if (input.nodeValue.length === 0) {
+    alert("Please Enter Something to add ! ");
+    btn.addEventListener("click", stopAdding());
+  } else if (input.nodeValue.length >= 40) {
+    alert("Maximum value of letters is 40 !");
+    btn.addEventListener("click", stopAdding());
+  }
+}
